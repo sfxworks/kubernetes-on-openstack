d9fa402509a47518ab64ea77fb1d5d7f5a7f8f25
Samuel Walker <sfxworks@gmail.com>
2019-10-27 18:30:37 -0400

    Should be using MultipartMime but thats still a research in progress

--- packer/k8s-configured/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-sudo mkdir -p /etc/kubernetes
-sudo mkdir -p /var/lib/kubelet
-sudo mv cloud-config /etc/kubernetes
-sudo mv kubelet-extra-args.env /etc/default/kubelet
-sudo systemctl daemon-reload
