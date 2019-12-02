6ce8092909edeed56f90e09d44d78cbf4635ce49
Samuel Walker <services@sfxworks.net>
2019-12-02 06:38:57 -0500

    Updated make to use cri-o

--- packer/k8s-base/make.sh
+++ packer/k8s-base/make.sh
@@ -2,11 +2,51 @@
 
 set -e
 
-sudo su <<HERE
-set -e
-#install kubernetes
+sudo apt update -y 
+sudo apt upgrade -y
 
-apt-get update && apt-get install -y apt-transport-https curl ipset
+sudo apt-get update -y 
+sudo apt-get install -y git golang-go libbtrfs-dev libassuan-dev libdevmapper-dev   libglib2.0-dev   libc6-dev   libgpgme11-dev   libgpg-error-dev   libseccomp-dev   libsystemd-dev   libselinux1-dev   pkg-config   go-md2man   libudev-dev   software-properties-common   gcc   make ipset
+
+mkdir work
+export GOPATH=/home/ubuntu/work
+
+#Make/Install runc
+go get github.com/opencontainers/runc
+cd $GOPATH/src/github.com/opencontainers/runc
+make BUILDTAGS='seccomp apparmor ambient'
+sudo make install
+cd ~
+
+#Install CRIO
+git clone https://github.com/cri-o/cri-o.git
+cd cri-o
+git checkout release-1.16
+make BUILDTAGS='seccomp apparmor'
+sudo make install
+sudo make install.config
+sudo make install.systemd
+cd ..
+
+#Install CNI
+wget -q https://github.com/containernetworking/plugins/releases/download/v0.8.3/cni-plugins-linux-amd64-v0.8.3.tgz
+sudo mkdir -p /opt/cni/bin
+sudo tar -xvf cni-plugins-linux-amd64-v0.8.3.tgz -C /opt/cni/bin/
+
+#Install runsc
+wget -q https://storage.googleapis.com/gvisor/releases/nightly/latest/runsc
+sudo chmod a+x runsc
+sudo mv runsc /usr/local/bin
+
+#Make/Install conmon
+git clone https://github.com/containers/conmon.git
+cd conmon
+make
+sudo make install
+cd ..
+
+#Install kubernetes
+sudo su <<HERE
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
@@ -15,85 +55,16 @@ apt-get update
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
 
-#install containerd
-
-sudo mkdir -p \
-  /etc/cni/net.d \
-  /opt/cni/bin \
-  /etc/containerd
-
-wget -nv https://github.com/containernetworking/plugins/releases/download/v0.8.2/cni-plugins-linux-amd64-v0.8.2.tgz
-wget -nv https://github.com/containerd/containerd/releases/download/v1.3.0/containerd-1.3.0.linux-amd64.tar.gz
-wget -nv https://storage.googleapis.com/gvisor/releases/nightly/latest/runsc
-wget -nv https://github.com/opencontainers/runc/releases/download/v1.0.0-rc9/runc.amd64
-
-mv runc.amd64 /usr/bin/runc
-chmod +x /usr/bin/runc
-
-mv runsc /usr/local/bin/runsc
-chmod +x /usr/local/bin/runsc
-
-cat <<EOF | sudo tee /etc/systemd/system/containerd.service
-[Unit]
-Description=containerd container runtime
-Documentation=https://containerd.io
-After=network.target
-[Service]
-ExecStartPre=/sbin/modprobe overlay
-ExecStart=/bin/containerd
-Restart=always
-RestartSec=5
-Delegate=yes
-KillMode=process
-OOMScoreAdjust=-999
-LimitNOFILE=1048576
-LimitNPROC=infinity
-LimitCORE=infinity
-[Install]
-WantedBy=multi-user.target
-EOF
-
-tar -xvf cni-plugins-linux-amd64-v0.8.2.tgz -C /opt/cni/bin/
-tar -xvf containerd-1.3.0.linux-amd64.tar.gz -C /
-
-
-#install gvisor-containerd-shim
-wget -nv -O gvisor-containerd-shim https://github.com/google/gvisor-containerd-shim/releases/download/v0.0.3/gvisor-containerd-shim.linux-amd64
-chmod +x gvisor-containerd-shim
-sudo mv gvisor-containerd-shim /usr/local/bin/gvisor-containerd-shim
-
-#configure containerd
-cat <<EOF | sudo tee /etc/containerd/gvisor-containerd-shim.toml
-# This is the path to the default runc containerd-shim.
-runc_shim = "/usr/local/bin/containerd-shim"
-EOF
-
-cat <<EOF | sudo tee /etc/containerd/config.toml
-disabled_plugins = ["restart"]
-[plugins.linux]
-  shim = "/usr/local/bin/gvisor-containerd-shim"
-  shim_debug = true
-[plugins.cri.containerd.runtimes.runsc]
-  runtime_type = "io.containerd.runtime.v1.linux"
-  runtime_engine = "/usr/local/bin/runsc"
-  runtime_root = "/run/containerd/runsc"
-EOF
-
-
 echo br_netfilter >> /etc/modules
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 echo '1' > /proc/sys/net/ipv4/ip_forward
 modprobe br_netfilter
-
-
-systemctl enable kubelet
-systemctl enable containerd
-
-systemctl start containerd
-swapoff -a
-
-kubeadm config images pull --cri-socket=/var/run/containerd/containerd.sock --v=5
-
+echo 'KUBELET_EXTRA_ARGS="--fail-swap-on=false --cgroup-driver=systemd"' > /etc/default/kubelet
 HERE
 
-
+#Cleanup
+sudo apt remove -y git golang-go libassuan-dev libbtrfs-dev libdevmapper-dev   libglib2.0-dev   libc6-dev   libgpgme11-dev   libgpg-error-dev   libseccomp-dev   libsystemd-dev   libselinux1-dev   pkg-config   go-md2man   libudev-dev   software-properties-common   gcc   make
+rm -rf conmon
+sudo rm -rf cri-o
+sudo rm -rf work
+rm -rf cni-plugins-linux-amd64-v0.8.3.tgz
